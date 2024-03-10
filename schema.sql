cee1ab45d6e8c13dc4b85ce5b32434b41bec79b7
Bavisetti Pavan Kalyan <135210660+Pavan-kalyan99@users.noreply.github.com>
2024-03-10 15:59:13 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,48 @@
+
+CREATE TABLE product (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    desc TEXT,
+    SKU VARCHAR(255),
+    category_id INT,
+    inventory_id INT,
+    price DECIMAL(10,2) NOT NULL,
+    discount_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES product_category(id),
+    FOREIGN KEY (inventory_id) REFERENCES product_inventory(id),
+    FOREIGN KEY (discount_id) REFERENCES discount(id)
+);
+
+
+CREATE TABLE product_category (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    desc TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE product_inventory (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    quantity INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE discount (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    desc TEXT,
+    discount_percent DECIMAL(5,2) NOT NULL,
+    active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+
